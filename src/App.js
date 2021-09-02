e61a28b83e930a2e41fd4e80bb49b2515164eee4
Shubham8968 <Shivamno1420@gmail.com>
2021-09-02 18:35:05 +0530

    Redux complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {increment,decrement} from  './actions';
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dipatch = useDispatch();
   return (
     <div className="App">
-      <h2> HELLO </h2>
+      <h1> Counter {counter} </h1>
+      <button onClick={() => dipatch(increment(3))}> + </button>
+      <button  onClick={() => dipatch(decrement())}> - </button>
+
+     {isLogged ?  <h3> Valuable Information I shouldn't see </h3> : ' '}
     </div>
   );
 }
